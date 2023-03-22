13151a2553a93fff7a18eab5580a3baeab50d071
Ashish Sapkota <aacspkt3@gmail.com>
2023-03-22 17:03:39 +0545

    add spl transfer

--- programs/batch-transfer/src/lib.rs
+++ programs/batch-transfer/src/lib.rs
@@ -1,15 +1,151 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken, 
+    token::{
+        TokenAccount, 
+        Mint, 
+        Token, 
+        Transfer as TokenTransfer, 
+        transfer as token_transfer
+    }, 
+}; 
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod batch_transfer {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn deposit_token(ctx: Context<DepositToken>, amount: u64) -> Result<()> {
+        let token_program = ctx.accounts.token_program.to_account_info();
+        let from = ctx.accounts.from.to_account_info();
+        let to = ctx.accounts.vault.to_account_info();
+        let authority =ctx.accounts.authority.to_account_info();
+
+        ctx.accounts.ledger.authority = ctx.accounts.authority.key();
+
+        let accounts = TokenTransfer {
+            from,
+            to,
+            authority 
+        };
+
+        let ctx = CpiContext::new(token_program, accounts);
+        
+        token_transfer(ctx, amount)
+    }
+
+
+    pub fn spl_transfer(ctx: Context<SplTransfer>, amount: u64) -> Result<()> {
+        let token_program = ctx.accounts.token_program.to_account_info();
+        let from = ctx.accounts.vault.to_account_info();
+        let to = ctx.accounts.to.to_account_info();
+        let authority = ctx.accounts.authority.to_account_info();
+        
+        let vault_amount = ctx.accounts.vault.amount;
+        assert!(vault_amount > amount, "Vault amount is less than transfer amount");
+
+        let accounts = TokenTransfer {
+            from,
+            to,
+            authority 
+        };
+
+        let authority_key = ctx.accounts.authority.key();
+        let bump = ctx.bumps
+            .get("ledger")
+            .unwrap_or_else(|| panic!("Bump is missing."))
+            .to_be_bytes();
+        let signer_seeds: &[&[&[u8]]] = &[&[b"Ledger", authority_key.as_ref(), bump.as_ref()]];
+
+        let ctx = CpiContext::new_with_signer(
+            token_program,
+            accounts,
+            signer_seeds
+        );
+        
+        token_transfer(ctx, amount)
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct DepositToken<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = authority,
+    )]
+    pub from : Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = authority,
+        seeds = [b"Ledger", authority.key().as_ref()],
+        bump,
+        space = 8 + 32
+    )]
+    pub ledger: Account<'info, RegistrationLedger>,
+    
+    #[account(
+        init,
+        payer = authority,
+        associated_token::mint = mint,
+        associated_token::authority = ledger,
+    )]
+    pub vault: Account<'info, TokenAccount>,
+    
+    pub mint: Account<'info, Mint>,
+
+    pub token_program: Program<'info, Token>,
+    
+    pub system_program: Program<'info, System>,
+    
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+#[account]
+pub struct RegistrationLedger {
+    authority: Pubkey,
+}
+
+
+#[derive(Accounts)]
+pub struct SplTransfer<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    
+    #[account(
+        seeds = [b"Ledger", authority.key().as_ref()],
+        bump,
+    )]
+    pub ledger: Account<'info, RegistrationLedger>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = ledger,
+    )]
+    pub vault: Account<'info, TokenAccount>,
+    
+    pub to_owner: AccountInfo<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = authority,
+        associated_token::mint = mint,
+        associated_token::authority = to_owner,
+    )]
+    pub to: Account<'info, TokenAccount>,
+
+    pub mint: Account<'info, Mint>,
+
+    pub token_program: Program<'info, Token>,
+
+    pub system_program: Program<'info, System>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
