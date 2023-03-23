162ce7725111eb53bb1cc08814894d437d39643d
shrestha-roshan <shrestharoshan768@gmail.com>
2023-03-23 08:13:03 +0545

    Batch Sol Transfer

--- programs/batch-transfer/src/lib.rs
+++ programs/batch-transfer/src/lib.rs
@@ -1,15 +1,38 @@
 use anchor_lang::prelude::*;
-
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+use anchor_lang::solana_program::program::invoke;
+use anchor_lang::solana_program::system_instruction::transfer;
+declare_id!("FddwD1WwcAN5XfR3bzD9jW1wHs4Bg1jz7YXTMicshzZp");
 
 #[program]
 pub mod batch_transfer {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn batch_sol_transfer(ctx: Context<BatchSolTransfer>, amount: Vec<u64>) -> Result<()> {
+        msg!("Batch transfer solana token");
+        let from = &ctx.accounts.from;
+        let receivers = ctx.remaining_accounts.clone();
+
+        let mut ixs = vec![];
+        for (i, receiver) in receivers.iter().enumerate() {
+            if i < receivers.len() - 2 {
+                let ix = transfer(&from.key, &receiver.key, amount[i]);
+                ixs.push(ix);
+            }
+        }
+        msg!("invoking");
+        for ix in ixs.iter() {
+            invoke(&ix, receivers)?;
+        }
         Ok(())
     }
 }
+#[derive(Clone, Accounts)]
 
-#[derive(Accounts)]
-pub struct Initialize {}
+pub struct BatchSolTransfer<'info> {
+    #[account(signer)]
+    /// CHECK:
+    pub from: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
--- tests/batch-transfer.ts
+++ tests/batch-transfer.ts
@@ -2,15 +2,69 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { BatchTransfer } from "../target/types/batch_transfer";
 
-describe("batch-transfer", () => {
+describe("batch-transfer", async () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.BatchTransfer as Program<BatchTransfer>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const owner_buffer = [
+    199, 70, 8, 99, 248, 66, 30, 8, 75, 69, 185, 133, 180, 74, 109, 145, 120,
+    169, 57, 112, 248, 44, 107, 35, 88, 237, 151, 106, 224, 78, 206, 131, 119,
+    249, 244, 237, 207, 161, 59, 99, 84, 105, 11, 179, 7, 76, 68, 230, 58, 65,
+    232, 121, 248, 108, 113, 162, 88, 226, 92, 40, 215, 99, 140, 220,
+  ];
+
+  const owner = anchor.web3.Keypair.fromSecretKey(Buffer.from(owner_buffer));
+
+  it("Bulk Transfer", async () => {
+    // generate 10 random accounts
+    const random_accounts: anchor.web3.PublicKey[] = [];
+    const amounts: anchor.BN[] = [];
+
+    for (let i = 0; i < 20; i++) {
+      random_accounts.push(anchor.web3.Keypair.generate().publicKey);
+      amounts.push(new anchor.BN(100000000));
+    }
+    //Array of accout metas
+    const accounts: anchor.web3.AccountMeta[] = [];
+    for (const account of random_accounts) {
+      accounts.push({
+        pubkey: account,
+        isSigner: false,
+        isWritable: true,
+      });
+    }
+    // request air drop for all accounts
+    for (const account of random_accounts) {
+      console.log("airdrop");
+      anchor.getProvider().connection.requestAirdrop(account, 1000);
+    }
+
+    accounts.push(
+      {
+        pubkey: owner.publicKey,
+        isSigner: true,
+        isWritable: true,
+      },
+      {
+        pubkey: anchor.web3.SystemProgram.programId,
+        isSigner: false,
+        isWritable: false,
+      }
+    );
+
+    // call the program
+    const sig = await program.methods
+      .batchSolTransfer(amounts)
+      .accounts({
+        from: owner.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      })
+      .signers([owner])
+      .remainingAccounts(accounts)
+      .rpc();
+    console.log(sig);
   });
 });
