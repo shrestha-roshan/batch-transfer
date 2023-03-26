52797c6a40367a55d6fd62a5dce1b2b252b45f68
shrestha-roshan <shrestharoshan768@gmail.com>
2023-03-26 08:26:19 +0545

    Add batch sol/token transfer from PDA

--- programs/batch-transfer/src/lib.rs
+++ programs/batch-transfer/src/lib.rs
@@ -1,59 +1,159 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::program::invoke;
 use anchor_lang::solana_program::system_instruction::transfer;
+use anchor_spl::token::{Mint, Token, TokenAccount};
+use spl_token::instruction::transfer as spl_transfer;
 declare_id!("FddwD1WwcAN5XfR3bzD9jW1wHs4Bg1jz7YXTMicshzZp");
+
+// seeds
+const BATCH_TOKEN: &[u8] = b"batch-token";
+const BATCH_SOL: &[u8] = b"batch-sol";
 
 #[program]
 pub mod batch_transfer {
 
+    use anchor_lang::solana_program::program::invoke_signed;
+
     use super::*;
+
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+    pub fn initialize_sol(_ctx: Context<InitializeSolAccount>) -> Result<()> {
+        Ok(())
+    }
 
     pub fn batch_sol_transfer(ctx: Context<BatchSolTransfer>, amount: Vec<u64>) -> Result<()> {
         msg!("Batch transfer solana token");
         let from = &ctx.accounts.from;
+        msg!("from: {:?}", from.key);
         let receivers = ctx.remaining_accounts.clone();
 
         let mut ixs = vec![];
         for (i, receiver) in receivers.iter().enumerate() {
-            if i < receivers.len() - 2 {
-                let ix = transfer(&from.key, &receiver.key, amount[i]);
+            if i < receivers.len() - 1 {
+                let ix = transfer(&from.key(), &receiver.key, amount[i]);
                 ixs.push(ix);
             }
         }
         msg!("invoking");
+        let authority_key = ctx.accounts.from_authority.key();
+        let bump = ctx
+            .bumps
+            .get("from")
+            .unwrap_or_else(|| panic!("Bump is missing."))
+            .to_be_bytes();
+        let signer_seeds: &[&[&[u8]]] = &[&[b"batch-test", authority_key.as_ref(), bump.as_ref()]];
         for ix in ixs.iter() {
-            invoke(&ix, receivers)?;
+            invoke_signed(&ix, receivers, signer_seeds)?;
         }
         Ok(())
     }
 
-    pub fn sol_transfer(ctx: Context<SolTransfer>, amount: u64) -> Result<()> {
-        msg!("Transfer solana token");
+    pub fn batch_token_transfer(ctx: Context<BatchTokenTransfer>, amount: Vec<u64>) -> Result<()> {
+        msg!("Batch transfer token");
         let from = &ctx.accounts.from;
-        let receiver = &ctx.accounts.to;
-        let ix = transfer(&from.key, &receiver.key, amount);
-        invoke(&ix, &[from.clone(), receiver.clone()])?;
+        let receivers = ctx.remaining_accounts;
+
+        let mut ixs = vec![];
+        for (i, receiver) in receivers.iter().enumerate() {
+            if i < receivers.len() - 3 {
+                let ix = spl_transfer(
+                    &spl_token::id(),
+                    &from.key(),
+                    &receiver.key,
+                    &ctx.accounts.from_pda.key(),
+                    &[],
+                    amount[i],
+                )?;
+                ixs.push(ix);
+            }
+        }
+        msg!("invoking");
+        let authority_key = ctx.accounts.from_authority.key();
+        let bump = ctx
+            .bumps
+            .get("from_pda")
+            .unwrap_or_else(|| panic!("Bump is missing."))
+            .to_be_bytes();
+        let signer_seeds: &[&[&[u8]]] = &[&[BATCH_TOKEN, authority_key.as_ref(), bump.as_ref()]];
+        for ix in ixs.iter() {
+            //invoke
+            invoke_signed(&ix, receivers, signer_seeds)?;
+        }
         Ok(())
     }
 }
-#[derive(Clone, Accounts)]
 
+#[derive(Clone, Accounts)]
 pub struct BatchSolTransfer<'info> {
-    #[account(signer)]
-    /// CHECK:
+    #[account(
+        seeds = [b"batch-test", from_authority.key().as_ref()],
+        bump,
+    )]
+    ///CHECK::
     pub from: AccountInfo<'info>,
+    ///CHECK::
+    pub from_authority: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
 }
 
 #[derive(Clone, Accounts)]
-pub struct SolTransfer<'info> {
-    #[account(signer)]
-    /// CHECK:
-    pub from: AccountInfo<'info>,
+pub struct BatchTokenTransfer<'info> {
+    #[account(
+        init_if_needed,
+        payer = from_authority,
+        space = 8,
+        seeds = [BATCH_TOKEN, from_authority.key().as_ref()],
+        bump,
+    )]
+    ///CHECK::
+    pub from_pda: Box<Account<'info, BatchPda>>,
+    ///CHECK::
     #[account(mut)]
-    /// CHECK:
-    pub to: AccountInfo<'info>,
-    pub system_program: Program<'info, System>,
+    pub from_authority: AccountInfo<'info>,
+    #[account(
+        associated_token::mint = mint,
+        associated_token::authority = from_pda,
+    )]
+    pub from: Account<'info, TokenAccount>,
+    pub mint: Box<Account<'info, Mint>>,
+    pub token_program: Program<'info, Token>,
     pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
 }
+
+#[derive(Clone, Accounts)]
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        seeds = [BATCH_TOKEN, from_authority.key().as_ref()],
+        bump,
+        payer = from_authority,
+        space = 8
+    )]
+    pub from_pda: Box<Account<'info, BatchPda>>,
+    #[account(signer, mut)]
+    ///CHECK::
+    pub from_authority: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Clone, Accounts)]
+pub struct InitializeSolAccount<'info> {
+    #[account(
+        init,
+        seeds = [b"batch-test", from_authority.key().as_ref()],
+        bump,
+        payer = from_authority,
+        space = 0
+    )]
+    ///CHECK::
+    pub from_pda: AccountInfo<'info>,
+    #[account(signer, mut)]
+    ///CHECK::
+    pub from_authority: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
+#[account]
+pub struct BatchPda {}
--- tests/batch-transfer-contract-1.ts
+++ tests/batch-transfer-contract-1.ts
@@ -1,5 +1,11 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import {
+  createAssociatedTokenAccount,
+  createTransferCheckedInstruction,
+  getAssociatedTokenAddressSync,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
 import { BatchTransfer } from "../target/types/batch_transfer";
 
 describe("batch-transfer", async () => {
@@ -7,6 +13,7 @@ describe("batch-transfer", async () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.BatchTransfer as Program<BatchTransfer>;
+  const connection = anchor.getProvider().connection;
 
   const owner_buffer = [
     199, 70, 8, 99, 248, 66, 30, 8, 75, 69, 185, 133, 180, 74, 109, 145, 120,
@@ -14,10 +21,31 @@ describe("batch-transfer", async () => {
     249, 244, 237, 207, 161, 59, 99, 84, 105, 11, 179, 7, 76, 68, 230, 58, 65,
     232, 121, 248, 108, 113, 162, 88, 226, 92, 40, 215, 99, 140, 220,
   ];
-
+  const mint = new anchor.web3.PublicKey(
+    "9xLaH2mj5mX1apnPdWfaU24x5sR9QF1wPU6gbZBAgorf"
+  );
   const owner = anchor.web3.Keypair.fromSecretKey(Buffer.from(owner_buffer));
 
   it("Bulk Transfer", async () => {
+    const owner_pda = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("batch-test"), owner.publicKey.toBuffer()],
+      program.programId
+    );
+    console.log("ownder-pda", owner_pda[0].toBase58());
+    try {
+      await program.methods
+        .initializeSol()
+        .accounts({
+          fromAuthority: owner.publicKey,
+          fromPda: owner_pda[0],
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([owner])
+        .rpc();
+    } catch (e) {
+      console.log(e);
+    }
+
     // generate 10 random accounts
     const random_accounts: anchor.web3.PublicKey[] = [];
     const amounts: anchor.BN[] = [];
@@ -42,30 +70,117 @@ describe("batch-transfer", async () => {
       anchor.getProvider().connection.requestAirdrop(account, 1000);
     }
 
+    accounts.push({
+      pubkey: owner_pda[0],
+      isSigner: false,
+      isWritable: true,
+    });
+
+    try {
+      // call the program
+      const sig = await program.methods
+        .batchSolTransfer(amounts)
+        .accounts({
+          from: owner_pda[0],
+          fromAuthority: owner.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        })
+        .signers([])
+        .remainingAccounts(accounts)
+        .rpc();
+      console.log(sig);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  it("Bulk Transfer", async () => {
+    const owner_pda = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("batch-token"), owner.publicKey.toBuffer()],
+      program.programId
+    );
+    console.log("owner-pda", owner_pda[0].toBase58());
+    // await program.methods
+    //   .initialize()
+    //   .accounts({
+    //     fromAuthority: owner.publicKey,
+    //     fromPda: ownder_pda[0],
+    //     systemProgram: anchor.web3.SystemProgram.programId,
+    //   })
+    //   .signers([owner])
+    //   .rpc();
+
+    const owner_ata_pda = getAssociatedTokenAddressSync(
+      mint,
+      owner_pda[0],
+      true
+    );
+    // generate 10 random accounts
+    const random_accounts_ata: anchor.web3.PublicKey[] = [];
+    const amounts: anchor.BN[] = [];
+    await createAssociatedTokenAccount(connection, owner, mint, owner_pda[0]);
+
+    // max 19 accounts
+    for (let i = 0; i < 19; i++) {
+      let account = anchor.web3.Keypair.generate();
+      let ata = await createAssociatedTokenAccount(
+        connection,
+        owner,
+        mint,
+        account.publicKey
+      );
+      random_accounts_ata.push(ata);
+      amounts.push(new anchor.BN(100000000));
+    }
+
+    //Array of accout metas
+    const accounts: anchor.web3.AccountMeta[] = [];
+    for (const account of random_accounts_ata) {
+      accounts.push({
+        pubkey: account,
+        isSigner: false,
+        isWritable: true,
+      });
+    }
+
     accounts.push(
       {
-        pubkey: owner.publicKey,
-        isSigner: true,
+        pubkey: owner_pda[0],
+        isSigner: false,
         isWritable: true,
       },
       {
-        pubkey: anchor.web3.SystemProgram.programId,
+        pubkey: owner_ata_pda,
+        isSigner: false,
+        isWritable: true,
+      },
+      {
+        pubkey: TOKEN_PROGRAM_ID,
         isSigner: false,
         isWritable: false,
       }
     );
 
-    // call the program
-    const sig = await program.methods
-      .batchSolTransfer(amounts)
-      .accounts({
-        from: owner.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-      })
-      .signers([owner])
-      .remainingAccounts(accounts)
-      .rpc();
-    console.log(sig);
+    try {
+      // call the program
+      const sig = await program.methods
+        .batchTokenTransfer(amounts)
+        .accounts({
+          from: owner_ata_pda,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          fromAuthority: owner.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          fromPda: owner_pda[0],
+          mint: mint,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([])
+        .remainingAccounts(accounts)
+        .rpc();
+      console.log(sig);
+    } catch (e) {
+      console.log(e);
+    }
   });
 });
